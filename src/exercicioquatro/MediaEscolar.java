fc7cda606b3691bb5de6e1d51b11455046fe05c2
caiocroccia22 <caiocroccia22@gmail.com>
2026-01-13 22:11:05 -0300

    feat: implement class to calculate average of students

---
+++ src/exercicioquatro/MediaEscolar.java
@@ -0,0 +1,48 @@
+package exercicioquatro;
+
+import java.util.Scanner;
+
+public class MediaEscolar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MediaEscolar me = new MediaEscolar();
+		int[] notas = me.pegarNotas();
+		int media   = me.calculaMedia(notas);
+		me.verificaSePassou(media);
+	}
+	
+	public static int[] pegarNotas() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Digite a primeira nota\n");
+		int num1 = scan.nextInt();
+		System.out.print("Digite a segunda nota\n");
+		int num2 = scan.nextInt();
+		System.out.print("Digite a terceira nota\n");
+		int num3 = scan.nextInt();
+		System.out.print("Digite a quarta nota\n");
+		int num4 = scan.nextInt();
+		int[] arr = {num1, num2, num3, num4};
+		return arr;
+	}
+	
+	public static int calculaMedia(int[] notas) {
+		int soma = 0;
+		for(int i = 0; i < notas.length; i++) {
+			soma += notas[i];
+			
+		}
+		int media = soma / 4;
+		System.out.print("Sua média é" + " " + media + "\n");
+		return media;
+	}
+	
+	public static void verificaSePassou(int media) {
+		if (media >= 7) {
+			System.out.print("Parabénsss, você passou!!");
+		} else {
+			System.out.print("Você não passou de ano");
+		}
+	}
+
+}
